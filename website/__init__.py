3a04075938c5e3576535d2231a4bfe1ccc148011
darthnall <blakenall@proton.me>
2023-09-22 20:46:13 -0500

    Fixed circular import

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from .db import db, db_url
 import os
 
 global app
 app = Flask(__name__, template_folder=os.path.abspath('./templates'))
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 
+from .db import db, db_url
 db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 
 from website.public.routes import client_bp
 app.register_blueprint(client_bp, url_prefix='')
